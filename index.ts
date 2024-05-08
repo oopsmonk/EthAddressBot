30b751df9efb2566373623e54f401c04d5971d3b
oopsoonk <462383+oopsmonk@users.noreply.github.com>
2024-05-09 00:27:30 +0800

    Implemented latestBlockWorker

--- index.ts
+++ index.ts
@@ -1,1 +1,118 @@
-console.log("Hello via Bun!");
+import { Web3 } from "web3";
+import figlet from "figlet";
+import addrList from "./targetAddresses.json";
+
+declare module "bun" {
+  interface Env {
+    RPC_PROVIDER: string;
+  }
+}
+
+interface AddressList {
+  name: string;
+  address: string;
+}
+
+interface Transaction {
+  blockHash: string;
+  blockNumber: bigint;
+  from: string;
+  gas: bigint;
+  gasPrice: bigint;
+  hash: string;
+  input: string;
+  nonce: bigint;
+  to: string;
+  transactionIndex: bigint;
+  value: bigint;
+  type: bigint;
+  chainId: bigint;
+  v: bigint;
+  r: string;
+  s: string;
+}
+
+const targetList: AddressList[] = addrList.target;
+const aliasList: AddressList[] = addrList.alias;
+const rpc = Bun.env.RPC_PROVIDER;
+
+function greeding(): boolean {
+  const welcome = figlet.textSync("* * Eth Address Logger * *");
+  console.log(welcome);
+  console.log("Build with Bun v" + Bun.version);
+  console.log("RPC Endpoint: " + rpc);
+
+  if (targetList.length == 0 || targetList == undefined) {
+    console.log("Invalid target address list!");
+    return false;
+  }
+
+  console.log("===== Monitoring Addresses =====");
+  targetList.forEach((a) => {
+    console.log(a.name + ": " + a.address);
+  });
+
+  if (aliasList.length > 0) {
+    console.log("========== Alias List ==========");
+    aliasList.forEach((a) => {
+      console.log(a.name + ": " + a.address);
+    });
+  }
+  return true;
+}
+
+if (rpc != undefined) {
+  if (greeding()) {
+    const web3 = new Web3(new Web3.providers.HttpProvider(rpc));
+    if (web3) {
+      console.log("starting worker....");
+      // const blockWorker = new Worker(
+      //   new URL("latestBlockWorker.ts", import.meta.url).href
+      // );
+      const blockWorker = new Worker("./latestBlockWorker.ts");
+
+      blockWorker.addEventListener("open", () => {
+        console.log("worker init");
+        blockWorker.postMessage({ nodeRPC: rpc });
+      });
+
+      // blockWorker.postMessage({ web3: web3 });
+      blockWorker.addEventListener("message", (event) => {
+        if (event.data.ready) {
+          console.log("worker ready to go!!");
+          blockWorker.postMessage({ start: true });
+        } else if (event.data.txs) {
+          const txs: Transaction[] = event.data.txs;
+          console.log("txs: " + txs.length);
+          txs.forEach((tx) => {
+            console.log(
+              "value: " +
+                Web3.utils.fromWei(tx.value, "ether") +
+                " ether , from: " +
+                tx.from +
+                " , to: " +
+                tx.to +
+                " , hash: " +
+                tx.hash
+            );
+          });
+        }
+      });
+    } else {
+      console.log("init web3 with endpoint failed: " + rpc);
+    }
+  }
+} else {
+  console.log("RPC_PROVIDER is not defined in .env file!");
+}
+
+// const server = Bun.serve({
+//   // defualt port is 3030
+//   port: Bun.env.PORT || 3030,
+//   fetch(req) {
+//     console.log("request from:" + req.headers.get("host"));
+//     return new Response("How! Bun! Bun!");
+//   },
+// });
+//
+// console.log(`Listening on http://localhost:${server.port} ...`);
---
+++ latestBlockWorker.ts
@@ -0,0 +1,32 @@
+import Web3 from "web3";
+
+// prevents TS errors
+declare var self: Worker;
+
+let web3: any = undefined;
+let latestBlock: bigint = 0n;
+self.addEventListener("message", async (event) => {
+  if (event.data.nodeRPC) {
+    web3 = new Web3(new Web3.providers.HttpProvider(event.data.nodeRPC));
+    if (web3) {
+      postMessage({ ready: true });
+    } else {
+      console.log("init work failed: " + event.data.nodeRPC);
+    }
+  } else if (event.data.start) {
+    console.log("start work....");
+    if (web3 == undefined) {
+      console.log("cannot init node PRC, terminat worker...");
+      process.exit();
+    }
+    while (true) {
+      const block = await web3.eth.getBlock("latest", true);
+      if (latestBlock != block.number) {
+        latestBlock = block.number;
+        console.log("new block: " + latestBlock);
+        postMessage({ txs: block.transactions });
+      }
+      Bun.sleepSync(2000);
+    }
+  }
+});
